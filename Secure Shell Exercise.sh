e954a6a145a69b23880565e9f2f3ea73ef1710f5
MBINGHORH <ymbinghorhndenge@gmail.com>
2024-07-10 05:33:57 +0100

    first commit on NEXUS

---
+++ Secure Shell Exercise.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+SSH: Secure Shell
+AUTOMATE WITH Shell Scripting
+
+pwd
+ls
+ls -a
+ls -la
+ls -lt
+ls -ltr
+
+whoami
+lslogins
+uptime
+
+mkdir MAGNETO
+cd MAGNETO/
+
+touch LORD-GOD-ALMIGHTY
+chmod 000 LORD-GOD-ALMIGHTY
+cat LORD-GOD-ALMIGHTY
+
+echo "IN THE BEGINNING
+WAS THE WORD and
+THE WORD WAS WITH GOD
+and THE WORD WAS GOD."
+
+##########################################################
+# Author: MAGNETO 007
+# Date: 25/05/2024
+#
+# This script outputs the node health
+#
+# Version: v1
+##########################################################
+
+
+df -h
+free -g
+nproc
+
+cat JESUS CHRIST IS THE ONLY TRUE MESSIAH ALSO KNOWN AS THE CHRIST!
+cat 2022
+date
+
+echo "BLESS THE LORD OH MY SOUL AND FORGET NOT ALL HIS BENEFITS!"
+
+sudo vi /etc/ssh/sshd_config
+********************************************************************************************************
+echo "UPDATE SSH.sh"
+********************************************************************************************************
+nproc
+free -g
+df -h
+date
+
+megatron@DESKTOP-A83FSGN:~$
+cat nodeHealth.sh
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+megatron@DESKTOP-A83FSGN:~$ cat nodeHealth.sh
+#!/bin/bash
+
+echo " PRAISE THE LORD and
+BLESS THE LORD OH MY SOUL
+and FORGET NOT ALL HIS BENEFITS."
+
+########################################################
+# Author: Megatron 1
+# Date: 25/05/2024
+#
+# This script outputs the node health.
+#
+# Version: v1
+########################################################
+
+set -x    # debug mode #
+
+echo "Print the disk space."
+df -h
+
+echo "Print memory usage."
+free -g
+
+echo "Print the CPU."
+nproc
+
+echo "THE PRINCIPLES OF THE
+LORD GOD ALMIGHTY and HIS
+EVERLASTING KINGDOM."
+
+cat 2022
+date
+call 2025
+callcall 2025
+call
+date
+
+sudo vi /etc/ssh/sshd_config
+uptime
+echo "WELCOME TO LIONTECH CANADA!"
+
+megatron@DESKTOP-A83FSGN:~$
